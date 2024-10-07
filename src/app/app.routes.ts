30fdb7baf0f74b65e000733d2c634794ab2be8ed
BRANDON <brandonescobedo1234@gmail.com>
2024-10-06 20:59:49 -0600

    b

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path:'', redirectTo:'/contacts', pathMatch:'full',   
+    },
+    {
+        path:'contacts',loadChildren:()=>import('./features/contacts/contacts.routes'),
+    },
+    {
+        path:'**',redirectTo:'/contacts'
+    }
+];
