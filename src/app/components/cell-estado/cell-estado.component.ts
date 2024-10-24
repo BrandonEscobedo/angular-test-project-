9078f9b4e030409963a29ec48f0142fedb7db267
BRANDON <brandonescobedo1234@gmail.com>
2024-10-23 21:06:37 -0600

    cambio de grid

---
+++ src/app/components/cell-estado/cell-estado.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cell-estado',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      cell-estado works!
+    </p>
+  `,
+  styles: ``
+})
+export class CellEstadoComponent {
+
+}
--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,16 +1,17 @@
-import { AfterViewInit, Component, effect, input, OnInit, signal, viewChild } from '@angular/core';
+import { AfterViewInit, Component, effect, Input, input, OnInit, signal, TemplateRef, viewChild } from '@angular/core';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { FilterComponent } from "./filter/filter.component";
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule, MatPaginatorModule, FilterComponent,MatIconModule,MatButtonModule],
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule, MatPaginatorModule, FilterComponent, MatIconModule, MatButtonModule,CommonModule],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.css'
 })
@@ -19,30 +20,36 @@ export class GridComponent<T> implements OnInit {
   // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   // dataSource = ELEMENT_DATA;
   constructor() {
-    effect(()=>{
-      if(this.valueToFilter()){
-        this.dataSource.filter=this.valueToFilter();
+    effect(() => {
+      if (this.valueToFilter()) {
+        this.dataSource.filter = this.valueToFilter();
       }
-      else{
-        this.dataSource.filter='';
+      else {
+        this.dataSource.filter = '';
       }
-    },{allowSignalWrites:true});
-   }
+    }, { allowSignalWrites: true });
+  }
 
-  displayedColumns=input.required<string[]>();
-  data=input.required<T[]>();
-  sortableColumns=input<string[]>([]);
-  private readonly _sort=viewChild.required<MatSort>(MatSort);
-  private readonly _paginator=viewChild.required<MatPaginator>(MatPaginator);
-  dataSource=new MatTableDataSource<T>();
-  valueToFilter=signal('');
+  displayedColumns = input.required<string[]>();
+  data = input.required<T[]>();
+  sortableColumns = input<string[]>([]);
+  @Input() headerMapping: { [key: string]: string } = {};
+  @Input() customCellTemplates: { [key: string]: TemplateRef<any> } = {};
+  private readonly _sort = viewChild.required<MatSort>(MatSort);
+  private readonly _paginator = viewChild.required<MatPaginator>(MatPaginator);
+  dataSource = new MatTableDataSource<T>();
+  valueToFilter = signal('');
   ngOnInit(): void {
-  this.dataSource.data=this.data();
-  this.dataSource.sort=this._sort();
-  this.dataSource.paginator=this._paginator();
+    this.dataSource.data = this.data();
+    this.dataSource.sort = this._sort();
+    this.dataSource.paginator = this._paginator();
   }
   applyFilter(event: Event): void {
-    const filterValue= (event.target as HTMLInputElement).value;
-    this.dataSource.filter=filterValue.trim().toLowerCase();
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  getHeaderLabel(column: string): string {
+    // Obtiene el valor del signal antes de indexarlo
+    return this.headerMapping[column] || column;
   }
 }
